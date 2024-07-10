fc5e2a9fdc54c4c46638ee7dbdc0f178a52b8d71
Khushi Tulsiyan <khushitulsiyan495@gmail.com>
2024-07-10 15:56:14 +0530

    design changes

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
+import axios from 'axios';
+import Search from './componenets/search';
+import User from './componenets/user';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const searchUsers = async(query) =>{
+    try {
+      const response = await axios.get(`https://api.github.com/users/${query}`);
+      setUser(response.data);
+    }
+    catch(error){
+      setUser(null);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search searchUsers={searchUsers} />
+      <User user = {user} />
     </div>
   );
-}
+};
 
 export default App;
